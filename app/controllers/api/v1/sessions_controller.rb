4022f7782fc55e604204b9849d8fcbc847ef392d
john million <jpmillion@comcast.net>
2021-07-16 14:42:58 -0400

    add create method in sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::SessionsController < ApplicationController
+    def create
+        user = User.find_by(email: params[:email])
+        if customer && customer.authenticate(params[:password])
+            render json: { user: UserSerializer.new(user) }
+        else
+            render json: { errors: 'Invalid email or password' }
+        end
+    end
 end
