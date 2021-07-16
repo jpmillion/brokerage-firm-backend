40b38c3c5fa20c5c89a7dc5e2a162dbba6d651f3
john million <jpmillion@comcast.net>
2021-07-16 00:25:07 -0400

    create database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, format: { with: /@/ }, uniqueness: { case_sensitive: false }, length: { minimum: 7, maximum: 254 }
+  validates :password, length: { in: 6..20 }
   has_secure_password
 end
