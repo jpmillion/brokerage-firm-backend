e36156446b872360e2e2f325f1de00283f17782b
john million <jpmillion@comcast.net>
2021-07-24 19:22:22 -0400

    Define User class method create_user_for_google

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  validates :first_name, :last_name, :email, presence: true
+  validates :email, presence: true
   validates :email, format: { with: /@/ }, uniqueness: { case_sensitive: false }, length: { minimum: 7, maximum: 254 }
-  validates :password, length: { in: 6..20 }
+  validates :password, length: { minimum: 6 }
   has_secure_password
+
+  def self.create_user_for_google(data)
+    name =  data['name'].split(' ')                 
+    user = User.find_or_create_by(email: data['email']) do |u|
+      u.first_name = name[0]
+      u.last_name = name[1]
+      u.password = SecureRandom.hex(12)
+    end
+  end 
 end
