214e72d26133f7af979504874475e5641fca5b69
john million <jpmillion@comcast.net>
2021-07-29 07:48:58 -0400

    Define methods jwt_key, issue_token, decoded_token for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+    def jwt_key
+        Rails.application.credentials.jwt_key
+    end
+
+    def issue_token(user)
+        JWT.encode({user_id: user.id}, jwt_key, 'HS256')
+    end
+
+    def decoded_token(token)
+        begin
+          JWT.decode(token, jwt_key, true, { :algorithm => 'HS256' })
+        rescue JWT::DecodeError
+          [{error: "Invalid Token"}]
+        end
+    end
 end
