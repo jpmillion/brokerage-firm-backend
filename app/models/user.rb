52d3d7169c2bc34b7b75fdddb3907ddbb58dde86
john million <jpmillion@comcast.net>
2021-08-15 20:55:06 -0400

    Add has_many and has_many through relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   validates :email, format: { with: /@/ }, uniqueness: { case_sensitive: false }, length: { minimum: 7, maximum: 254 }
   validates :password, length: { minimum: 6 }
   has_secure_password
+  has_many :user_accounts
+  has_many :account_types, through: :user_accounts
 
   def self.create_user_for_google(data)
     name =  data['name'].split(' ')                 
