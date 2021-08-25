261b62af66d0963700fd34e06d1daec062746fde
john million <jpmillion@comcast.net>
2021-08-25 19:46:40 -0400

    Test user can be created only if email and password valid

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should create new user' do 
+    user = User.new(email: 'jon@example.com', password: 'password')
+    assert user.save
+  end
+
+  test 'should not save without valid email or password' do
+    # email and password blank
+    user1 = User.new
+    assert_not user1.save
+
+    # invalid email format
+    user2 = User.new(email: 'sdflkdjsklaj', password: 'password')
+    assert_not user2.save
+
+    # invalid password length
+    user3 = User.new(email: 'jon@example.com', password: 'pass')
+    assert_not user3.save
+  end
 end
