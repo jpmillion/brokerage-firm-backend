e8f94d72f9cb2e297eed4e11c0ac3873be28b857
john million <jpmillion@comcast.net>
2021-08-25 20:46:34 -0400

    Assert account_type can be created

--- test/models/account_type_test.rb
+++ test/models/account_type_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class AccountTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should create instance of account type' do
+    account_type = AccountType.new(name: 'IRA', description: 'Retirement Account that can be withdrawn from at age 59 and 1/2')
+    assert account_type.save
+  end
 end
