f5cc85ab83c6838642d878ec8bf116f1454e3023
john million <jpmillion@comcast.net>
2021-08-25 20:48:07 -0400

    Assert user_account is created only when valid

--- test/models/user_account_test.rb
+++ test/models/user_account_test.rb
@@ -1,8 +1,23 @@
 require "test_helper"
 
 class UserAccountTest < ActiveSupport::TestCase
-  test "should not save without user or account type" do
+  account_type = AccountType.create(name: 'IRA', description: 'Retirement Account')
+  user = User.create(email: 'jan@example.com', password: 'ldska;fjsd;lafj')
+
+  test "should create user account" do
     account = UserAccount.new
-    assert_not account.save
+    account_type.user_accounts << account
+    user.user_accounts << account
+    assert account.valid?
+  end
+
+  test "should not save without user or account type" do
+    account1 = UserAccount.new
+    account_type.user_accounts << account1
+    assert_not account1.valid?
+
+    account2 = UserAccount.new
+    user.user_accounts << account2
+    assert_not account2.valid?
   end
 end
